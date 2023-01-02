725e3c7bc9ef159a725dbdc066ccc574dbcd95cc
Adarsh <jha01adarsh@gmail.com>
2023-01-02 13:51:40 +0530

    Type Conversion

---
+++ 2-typeCov and String/index.js
@@ -0,0 +1,20 @@
+console.log("hello");
+/*Type conversion */
+
+let a = 10;
+console.log(a); /*10 */
+console.log(typeof a); /*number */
+console.log(typeof String(a)); /* string*/
+
+let b = "Hello";
+console.log(b); /*Hello */
+console.log(typeof b); /*string */
+console.log(typeof Number(b)); /* number*/
+
+let c = 10;
+console.log(c); /*10 */
+console.log(typeof c); /*number */
+// console.log(typeof String(c)); 
+c = c.toString(c);
+console.log(typeof c);/* string*/
+
