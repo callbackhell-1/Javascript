00b5e6df07168954df50a22fcb507979e0270a0a
Adarsh <jha01adarsh@gmail.com>
2023-01-02 18:05:07 +0530

    Type Conversion & String Methods

--- 2-typeCov and String/index.js
+++ 2-typeCov and String/index.js
@@ -18,3 +18,29 @@ console.log(typeof c); /*number */
 c = c.toString(c);
 console.log(typeof c);/* string*/
 
+/* ----------String Methods --------*/
+
+let s = "My name is Adarsh"
+
+console.log(s[3]);
+console.log(s.indexOf("a"));
+console.log(s.lastIndexOf("a"));
+console.log(s.charAt("11"));
+console.log(s.endsWith("adarsh"));
+console.log(s.endsWith("is adarsh"));
+console.log(s.includes("adarsh"));
+console.log(s.includes("is adarsh"));
+console.log(s.substring(0,4));
+console.log(s.slice(0,4));
+console.log(s.split(" "));
+console.log(s.split("a"));
+console.log(s.replace("a","b"));
+console.log(s.length);
+console.log(s.toLocaleLowerCase());
+console.log(s.substr(0,4));
+
+/* ----------Diff b/w substr and subString --------*/
+
+console.log(s.substring(3,4));
+console.log(s.substr(3,4));
+
