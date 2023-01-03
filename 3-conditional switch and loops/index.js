0800916ca7126ea651c557d119a3ee50507f3ec4
Adarsh <jha01adarsh@gmail.com>
2023-01-03 12:55:54 +0530

    conditional statements, switch case & loops

---
+++ 3-conditional switch and loops/index.js
@@ -0,0 +1,46 @@
+/* --------------Conditional---------------------*/
+
+/*
+let price = prompt("Enter the price");
+console.log(price);
+
+price =  Number(price);
+
+console.log(typeof price);
+if (price === 10) {
+  console.log("Your icecream is vanilla");
+} else if (price === 20) {
+  console.log("Your icecream is choco");
+} else {
+  console.log("Enter valid Number");
+}
+*/
+
+/* --------------Switch Case---------------------*/
+
+/*
+let price1 = Number(prompt("Enter the price1"));
+
+switch (price1) {
+  case 10:
+    console.log("Your icecream is vanilla");
+    break;
+  case 20:
+    console.log("Your icecream is choco");
+    break;
+
+  default:
+    console.log("Enter valid Number");
+    break;
+}
+*/
+
+/* --------------LOOP ---------------------*/
+
+/* for loop */
+for (let i = 0; i <= 10; i++) {
+  console.log(Math.random());
+  console.log("Adarsh");
+}
+/*while loop */
+/* do while loop */
