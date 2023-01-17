72cc831d1f59195e13cb96b81cc27ec720c3880c
Adarsh <jha01adarsh@gmail.com>
2023-01-17 19:14:06 +0530

    Local storage

--- 11-Local Storage and Session Storage/index.js
+++ 11-Local Storage and Session Storage/index.js
@@ -3,7 +3,6 @@ const myInput = document.getElementById("myInput");
 const deleteBtn = document.getElementById("deleteBtn");
 
 //   /*-------------session storage INTRO------------ */
-
 {
   // const btnClicked = function () {
   //   console.log(myInput.value);
@@ -19,19 +18,49 @@ const deleteBtn = document.getElementById("deleteBtn");
   // console.log(sessionStorage.getItem("key")); /*value*/
   /* If we open same url in different tab e will get session storage key-value as null,bcoz scopeof this is only in this session */
 }
-
 /*------------------session storage Cont..----------------*/
+{
+  // const btnClicked = function () {
+  //   console.log(myInput.value);
+  //   /*-------session storage---- */
+  //   sessionStorage.setItem("Name", myInput.value);
+  // };
+  // myBtn.addEventListener("click", btnClicked);
+  // deleteBtn.addEventListener("click", () => {
+  //   //   console.log(sessionStorage.length);
+  //   //  console.log(sessionStorage.key(0));
+  //   //   sessionStorage.removeItem("Name");
+  //   sessionStorage.clear();
+  // });
+}
+/*------------------Local storage ---------------*/
 
-const btnClicked = function () {
-  console.log(myInput.value);
-  /*-------session storage---- */
-  sessionStorage.setItem("Name", myInput.value);
+const btnClick = function () {
+  /*It store in the form of string */
+  //   localStorage.setItem("Key1", myInput.value);
+
+  /*to store in the form of object */
+  //   localStorage.setItem("key1",{name:"Adarsh",Surname:"Jha"});
+
+  /*to convert object into string */
+  localStorage.setItem(
+    "key1",
+    JSON.stringify({ name: "Adarsh", Surname: "Jha" })
+  );
 };
-myBtn.addEventListener("click", btnClicked);
 
+myBtn.addEventListener("click", btnClick);
 deleteBtn.addEventListener("click", () => {
-  //   console.log(sessionStorage.length);
-  //  console.log(sessionStorage.key(0));
-  //   sessionStorage.removeItem("Name");
-  sessionStorage.clear();
+  localStorage.removeItem("key1");
 });
+
+console.log(
+  localStorage.getItem("key1")
+); /*{"name":"Adarsh","Surname":"Jha"} */
+
+console.log(
+  JSON.parse(localStorage.getItem("key1"))
+); /*{name: 'Adarsh', Surname: 'Jha'} */
+
+/* Local storage , will not change if  e chg the tab,close the browser */
+/* Local storage& session storage time is ~ 5/10mb */
