95c5cca3d906ef8234e0207248f6c9c82298f73d
Adarsh <jha01adarsh@gmail.com>
2023-01-21 13:58:54 +0530

    setInterval & clearInterval

--- 13-SetTimeout and SetInterval/index.js
+++ 13-SetTimeout and SetInterval/index.js
@@ -1,43 +1,61 @@
 // 1.
-const printName = (name) => {
-  console.log(`my Name is ${name}`);
-};
+// const printName = (name) => {
+//   console.log(`my Name is ${name}`);
+// };
 
-{
-  /*
-setTimeout(printName("Adarsh"), 5000); //This will give instant result , but we want after 5sec.so we do:
+// {
+//   /*
+// setTimeout(printName("Adarsh"), 5000); //This will give instant result , but we want after 5sec.so we do:
 
-// setTimeout(printName,5000,"argumentPassing"); ->tax
-*/
-}
+// // setTimeout(printName,5000,"argumentPassing"); ->tax
+// */
+// }
 
-setTimeout(printName, 5000, "Adarsh"); // It will console after 5 sec
+// setTimeout(printName, 5000, "Adarsh"); // It will console after 5 sec
 
-// 3.
+// // 3.
 
-setTimeout(
-  (name, gender) => {
-    console.log(`My Name is ${name} and gender is ${gender}`);
-  },
-  7000,
-  "John",
-  "Male"
-);
+// setTimeout(
+//   (name, gender) => {
+//     console.log(`My Name is ${name} and gender is ${gender}`);
+//   },
+//   7000,
+//   "John",
+//   "Male"
+// );
 
-/*---------------------clearTimeout----------------------- */
+// /*---------------------clearTimeout----------------------- */
 
-// 2.
+// // 2.
 
-const personDetails = (name, age, salary) => {
+// const personDetails = (name, age, salary) => {
+//   console.log(`my name is ${name} age is ${age} and salary is $${salary}`);
+// };
+
+// const passingId = setTimeout(personDetails, 4000, "Adarsh", 45, 500);
+
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", () => {
+//   // Applyng it on 2.
+//   //   clearTimeout(id); -> syntax
+//   clearTimeout(passingId);
+// });
+/* Now when we click on the button, then it will not console the (2) because we set clear timeout in it. And in clearTimeout we have pass an id */
+
+/*---------------------SetInterval----------------------- */
+
+const personDetails1 = (name, age, salary) => {
   console.log(`my name is ${name} age is ${age} and salary is $${salary}`);
 };
 
-const passingId = setTimeout(personDetails, 4000, "Adarsh", 45, 500);
+const passingId2 = setInterval(personDetails1, 4000, "Adarsh", 45, 500);
+
+//   It will keep on repeating after every 4 sec
+
+/*----------clearInterval------------- */
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
-  // Applyng it on 2.
-  //   clearTimeout(id); -> syntax
-  clearTimeout(passingId);
+  clearInterval(passingId2);
+  console.log("button clicked");
 });
-/* Now when we click on the button, then it will not console the (2) because we set clear timeout in it. And in clearTimeout we have pass an id */
